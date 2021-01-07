4df55709b32bb2f55278b9016e79c7012464bd71
Shamil <shagiradgers@gmail.com>
2021-01-08 04:08:09 +0500

    add new pic add check for collision del enemy that was out of screen spawn opponents add delay between moving player (axis x)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import sys
 from random import sample
+from time import time
 
 
 class Menu:
@@ -11,10 +12,13 @@ class Menu:
         self.games_started = False
         self.button = LoadSprites.load_image('button.png')
         self.button_size = self.button.get_size()
+        self.background = LoadSprites.load_image('menu_background.png')
 
     def draw_menu(self):
-        # button "start game"
+        # draw background
+        screen.blit(self.background, (0, 0))
 
+        # button "start player"
         screen.blit(
             self.button, (width // 2 - self.button_size[0] // 2
                           , width // 2 - self.button_size[1] // 2)
@@ -22,8 +26,7 @@ class Menu:
 
     def update(self, pos):
         # if user clicked to 'button'
-        if width // 2 + self.button_size[0] >= pos[0] >= \
-                width // 2 - self.button_size[0] and \
+        if width // 2 + self.button_size[0] >= pos[0] >= width // 2 - self.button_size[0] and \
                 height // 2 + self.button_size[1] >= \
                 pos[0] >= width // 2 - self.button_size[1]:
             self.games_started = True
@@ -64,6 +67,7 @@ class MainGame:
 
 class Player(pygame.sprite.Sprite):
     player_img = LoadSprites.load_image('car.png')
+    player_img = pygame.transform.scale(player_img, (90, 135))
 
     def __init__(self):
         super(Player, self).__init__(all_sprites)
@@ -79,13 +83,13 @@ class Player(pygame.sprite.Sprite):
 
 class Opponents(pygame.sprite.Sprite):
     opponent_img = LoadSprites.load_image('opponent.png')
+    opponent_img = pygame.transform.scale(opponent_img, (90, 135))
 
     def __init__(self, x):
         super(Opponents, self).__init__(all_sprites)
         self.image = self.opponent_img
         self.rect = self.image.get_rect()
         self.rect.center = x, -self.image.get_size()[1]
-        print(self.rect.center)
 
     def update(self):
         if not self.rect.x >= height:
@@ -101,15 +105,17 @@ if __name__ == '__main__':
 
     screen = pygame.display.set_mode(size=size)
     menu = Menu()
-    game = Player()
+    player = Player()
     background = LoadSprites()
     step_x = width // 4
     step_y = 10
+    start = time()
+    delay = 0.5
 
     pos_for_ops = [width // 4, width // 2, width - width // 4]
-    ops = []
+    opponents = []
     for pos in sample(pos_for_ops, 2):
-        ops.append(Opponents(pos))
+        opponents.append(Opponents(pos))
 
     while running:
         for event in pygame.event.get():
@@ -121,31 +127,53 @@ if __name__ == '__main__':
                 menu.update(event.pos)
 
             elif event.type == pygame.KEYDOWN and menu.games_started:
+                end = time()
+
                 if (event.key == pygame.K_d or event.key == ord('в')) \
-                        and game.rect.x + step_x + game.size['x'] <= width:
-                    game.rect.x += step_x
+                        and player.rect.x + step_x + player.size['x'] <= width and \
+                        end - start >= delay:
+                    player.rect.x += step_x
+                    start = time()
 
                 elif (event.key == pygame.K_a or event.key == ord('ф')) \
-                        and game.rect.x - step_x - game.size['x'] >= 0:
-                    game.rect.x -= step_x
+                        and player.rect.x - step_x - player.size['x'] >= 0 and \
+                        end - start >= delay:
+                    player.rect.x -= step_x
+                    start = time()
 
                 elif (event.key == pygame.K_w or event.key == ord('ц')) \
-                        and game.rect.y - step_y - game.size['y'] >= height // 3:
-                    game.rect.y -= step_y
+                        and player.rect.y - step_y >= height // 3:
+                    player.rect.y -= step_y
+                    start = time()
 
                 elif (event.key == pygame.K_s or event.key == ord('ы')) \
-                        and game.rect.y + step_y + game.size['y'] <= height:
-                    game.rect.y += step_y
+                        and player.rect.y + step_y <= height:
+                    player.rect.y += step_y
+                    start = time()
 
         pygame.display.flip()
         if menu.games_started:
-            screen.fill('green')
             background.draw_ground()
-            game.update()
+            player.update()
             all_sprites.draw(screen)
             all_sprites.update()
         else:
             screen.fill('white')
             menu.draw_menu()
+
+        # add new opponents
+        if opponents[-1].rect.y >= height // 3 and opponents[-2].rect.y >= height // 3:
+            for pos in sample(pos_for_ops, 2):
+                opponents.append(Opponents(pos))
+
+        # del opponents that was out of screen
+        if opponents[0].rect.y >= height and opponents[0].rect.y >= height:
+            opponents.pop(0) and opponents.pop(0)
+
+        # rewrite this peace of code
+        for opponent in opponents:
+            if opponent.rect.center == player.rect.center:
+                print('CRUSH')
+
         clock.tick(fps)
     pygame.quit()
